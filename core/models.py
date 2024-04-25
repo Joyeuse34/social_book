6c85d060c9d8f2d3832ff2f4485f4f88b77601e7
Joyeuse34 <turinsanjar13@gmail.com>
2024-04-25 20:24:42 +0500

    add account settings

--- core/models.py
+++ core/models.py
@@ -8,7 +8,7 @@ class Profile(models.Model):
     id_user = models.IntegerField()
     bio = models.TextField(blank=True)
     profileimg = models.ImageField(upload_to='profile_images', default='blank-profile-picture.png')
-    location = models.CharField(max_length=100)
+    location = models.CharField(max_length=100, blank=True)
 
     def __str__(self):
-        return self.user.username
+        return (self.user.username)
