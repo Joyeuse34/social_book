77f51c2758426f5712d0e8119aa5c332c630900d
Joyeuse34 <turinsanjar13@gmail.com>
2024-04-26 15:59:35 +0500

    Upload post

--- core/urls.py
+++ core/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('signup/', views.signup, name='signup'),
     path('settings', views.settings, name='settings'),
+    path('upload', views.upload, name='upload'),
     path('signin', views.signin, name='signin'),
     path('logout', views.logout, name='logout')
 ]
