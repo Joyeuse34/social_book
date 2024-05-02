dcd880ac2d7f18502abad50df4a672dc18d20d62
Joyeuse34 <turinsanjar13@gmail.com>
2024-05-02 11:13:51 +0500

    Add follow and unfollow

--- core/urls.py
+++ core/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('settings', views.settings, name='settings'),
     path('upload', views.upload, name='upload'),
     path('profile/<str:pk>', views.profile, name='profile'),
+    path('follow', views.follow, name='follow'),
     path('signin', views.signin, name='signin'),
     path('logout', views.logout, name='logout'),
     path('like-post',views.like_post, name='like-post')
