1e73d93371d42636cfbe525df38844b7ffaede52
Joyeuse34 <turinsanjar13@gmail.com>
2024-05-04 19:26:40 +0500

    User suggestion

--- core/urls.py
+++ core/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('follow', views.follow, name='follow'),
     path('signin', views.signin, name='signin'),
     path('logout', views.logout, name='logout'),
-    path('like-post',views.like_post, name='like-post')
+    path('like-post',views.like_post, name='like-post'),
+    path('search', views.search, name='search')
 ]
